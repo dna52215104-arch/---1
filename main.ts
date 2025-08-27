73d214429c2e2213287ea32a479583659f867a8d
dna52215104-arch <dna52_215104@gclass.ice.go.kr>
2025-08-27 09:51:22 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(3)
+    basic.pause(500)
+    basic.showNumber(2)
+    basic.pause(500)
+    basic.showNumber(1)
+    basic.pause(500)
+    basic.clearScreen()
+    폭탄시간 = randint(5, 20)
+})
+let 폭탄시간 = 0
+폭탄시간 = -1
+radio.setGroup(1)
