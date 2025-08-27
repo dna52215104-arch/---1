dab37e4e79994b125d3b916c08816579d139ebae
dna52215104-arch <dna52_215104@gclass.ice.go.kr>
2025-08-27 10:03:41 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    폭탄시간 = receivedNumber
+})
+input.onGesture(Gesture.Shake, function () {
+    if (폭탄시간 > 0) {
+        radio.sendNumber(폭탄시간)
+        폭탄시간 = -1
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(3)
     basic.pause(500)
@@ -11,3 +20,16 @@ input.onButtonPressed(Button.AB, function () {
 let 폭탄시간 = 0
 폭탄시간 = -1
 radio.setGroup(1)
+basic.forever(function () {
+    if (폭탄시간 == 0) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(1000)
+    }
+    if (폭탄시간 < 0) {
+        basic.clearScreen()
+    }
+    if (폭탄시간 > 0) {
+        basic.showIcon(IconNames.Skull)
+        폭탄시간 += -1
+    }
+})
